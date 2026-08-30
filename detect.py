e39ff94eb1e79407680e57e6a9b396cde9314080
axom tools <adamowaissi055@gmail.com>
2026-08-31 00:33:29 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,4 +1,10 @@
 import re
 
 def matchcandidate(content):
-    return bool(re.search(r'v7\s*\(\s*"\\\d+', content) and "LOL!" not in content)
+    if re.search(r'LOL!', content):
+        return False
+    if re.search(r'math\.ldexp', content):
+        return False
+    if re.search(r'local\s+function\s+\w+\s*\(', content) and re.search(r'bxor', content) and re.search(r'string\.char', content):
+        return True
+    return False
