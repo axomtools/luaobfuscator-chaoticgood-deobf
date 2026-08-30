f70565a69f4d6355ebd4e27434a7a8cd83743274
axom tools <adamowaissi055@gmail.com>
2026-08-31 00:25:46 +0300

    Update detect.py

--- detect.py
+++ detect.py
@@ -2,23 +2,3 @@ import re
 
 def matchcandidate(content):
     return bool(re.search(r'v7\s*\(\s*"\\\d+', content) and "LOL!" not in content)
-
-def matchcandidateevil(content):
-    if re.search(r'LOL!', content):
-        return True
-    if re.search(r'math\.ldexp', content):
-        return True
-    if re.search(r'return\s+v\d+\s*\(', content):
-        return True
-    if re.search(r'v\d+\s*\(\s*["\']LOL!', content):
-        return True
-    return False
-
-def matchcandidateold(content):
-    if re.search(r'local\s+v0\s*=\s*tonumber', content) and re.search(r'local\s+function\s+v15\s*\(', content):
-        return True
-    if re.search(r'local\s+function\s+v0\s*\(', content) and re.search(r'bxor', content) and re.search(r'loadstring', content):
-        return True
-    if re.search(r'local\s+v0\s*=\s*string\.char', content) and re.search(r'loadstring', content):
-        return True
-    return False
